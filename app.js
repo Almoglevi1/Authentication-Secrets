5ea47a31617b8e3500503765727d138c5447e8f0
Almog Levi <almoglevi71@gmail.com>
2023-01-02 05:23:40 +0200

    Level 2 - Encryption + Add Environment Vars

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -11,54 +13,61 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB",
-{useNewUrlParser:true});
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
-  email:String,
-  password:String
-};
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
 
-const User = new mongoose.model("User",userSchema);
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
-app.get("/",function(req,res){
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function(req, res){
   res.render("login");
 });
 
-app.get("/register",function(req,res){
+app.get("/register", function(req, res){
   res.render("register");
 });
 
-app.post("/register",function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:req.body.password
+app.post("/register", function(req, res){
+  const newUser = new User ({
+    email: req.body.username,
+    password: req.body.password
   });
 
   newUser.save(function(err){
-    if(err){console.log(err);}
-    else{res.render("secrets");}
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
   });
 });
 
-app.post("/login",function(req,res){
-  const username=req.body.username;
-  const password=req.body.password;
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
 
-  User.findOne({email:username},function(err,foundUser){
-    if(err){console.log(err);}
-    else{if(foundUser){if(foundUser.password === password){
-      res.render("secrets");
-    }}}
-  })
-
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if(foundUser) {
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
 });
 
-//
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
